032a90ca1c8acda0a50b9dc12a739c2794192774
mgasoftware <macir.gassama12@gmail.com>
2023-08-14 16:57:27 +0200

    Add static root

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', views.MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
-    path('v1/item/', views.ItemList.as_view()),
+    path('v1/item/', views.ItemDetail.as_view()),
     path('v1/item/<int:pk>/', views.ItemDetail.as_view()),
     # path('v1/chat/', views.ConversationList.as_view()),
     # path('v1/chat/<int:pk>/', views.ConversationDetail.as_view()),
