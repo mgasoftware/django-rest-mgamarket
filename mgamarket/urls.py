513d17b2649fd6db3178455356cd99d981aa6ea0
mgasoftware <macir.gassama12@gmail.com>
2023-08-16 10:51:46 +0200

    Add a link to media files

--- mgamarket/urls.py
+++ mgamarket/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path("", include('core.urls')),
@@ -26,4 +28,7 @@ urlpatterns = [
     path("inbox/", include("conversation.urls")),
     path("admin/", admin.site.urls),
     path("api/", include('api.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
